efe94582b49d84cf1a18a69e4cd7e57fccabe673
Tobias Happ <tobias.happ@gmx.de>
2020-10-25 12:56:19 +0100

    Change deploy to prebuilt jar

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+./gradlew :function:shadowJar
+
 gcloud functions deploy test-function \
   --project functions-terraform \
   --region europe-west3 \
   --entry-point tobias.function.Function \
   --runtime java11 \
   --trigger-http \
-  --memory 512MB
+  --memory 512MB \
+  --source function/build/libs
