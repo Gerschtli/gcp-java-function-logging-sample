c985cfdf568f8e5ec62e762f1e24786263908c2f
Tobias Happ <tobias.happ@gmx.de>
2020-10-25 18:18:00 +0100

    Change project and function name

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 ./gradlew :function:shadowJar
 
-gcloud functions deploy test-function \
+gcloud functions deploy function-logging-sample \
   --project functions-terraform \
   --region europe-west3 \
   --entry-point tobias.function.Function \
